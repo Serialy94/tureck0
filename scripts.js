0e76aef5cb8a264caa3349bbb4fba86cffa75572
Serialy94 <tureckysvetserialov@gmail.com>
2025-03-03 17:43:49 +0000

    Update scripts.js
    Signed-off-by: Serialy94 <tureckysvetserialov@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
-$(document).ready(function(){
-    $(".movie-image").hover(function(){
-        $(this).find(".play").show();
-    },
-    function(){
-        $(this).find(".play").hide();
-    });
-
-    $(".blink").focus(function() {
-        if(this.title == this.value) {
-            this.value = '';
-        }
-    })
-    .blur(function(){
-        if(this.value == '') {
-            this.value = this.title;                    
-        }
-    });
+document.querySelector('button').addEventListener('click', function() {
+    alert('Netflix-style theme activated!');
 });
